0e430a454ba9851099748d2492986a4ada0fc67b
Dekana <chinenyeudechukwuu@gmail.com>
2022-08-01 21:33:00 +0100

    Emptied gmail and password

--- mycms/mycms/settings.py
+++ mycms/mycms/settings.py
@@ -130,5 +130,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "chinenyeudechukwuu@gmail.com"
-EMAIL_HOST_PASSWORD = "uca2002gmail"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
