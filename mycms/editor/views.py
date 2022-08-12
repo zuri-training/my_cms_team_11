b6f628609c1f747fa4fa489f5db48ebf301e093c
Dekana <chinenyeudechukwuu@gmail.com>
2022-08-12 11:17:11 +0100

    editor app file structe implemented (static and templates)

--- mycms/editor/views.py
+++ mycms/editor/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from webella.models import user_dashboard
+from django.contrib import messages
+from accounts.models import User
+# Create your views here.
+
+
+def index(request):
+    
+    
+    return render(request, 'hannah template/index.html')
 
 # Create your views here.
+
