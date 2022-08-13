f28980bc6bab573ebfd8fb647ad583c3e5a617c0
Dekana <chinenyeudechukwuu@gmail.com>
2022-08-13 14:22:50 +0100

    Figured out how to embed an iframe

--- mycms/editor/urls.py
+++ mycms/editor/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
 from . import views
+from django.views.generic import TemplateView
+from webella.models import user_dashboard
+
+
+
 
 app_name = 'editor'
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('preview', TemplateView.as_view(template_name="couple template/index.html"), name='preview'),
     path('view-website', views.webiste_template, name='webiste_template'),
     path('resume', views.hannah_resume, name='hannah_resume'),
     path('contact', views.hannah_contact, name='hannah_contact'),
