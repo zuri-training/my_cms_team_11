68d6c976978bf9cba16e07508e332acf731e530b
amin <owodunniadeyinka@gmail.com>
2022-08-19 23:48:16 +0100

    secrret key

--- mycms/settings.py
+++ mycms/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h3o19%mp&4i3y%)5b(cnij4irzjw_naw4_uhm4$z=hm0t**j_r'
+SECRET_KEY = config("SECRET_KEY" )
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR/'static']
 
